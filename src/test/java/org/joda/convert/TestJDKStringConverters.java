26548a51222c5a1c251e7d627395de559f540af1
Stephen Colebourne <scolebourne@joda.org>
2015-10-02 11:49:58 +0100

    More lenient test of InetAddress format
    Fixes #13

--- src/test/java/org/joda/convert/TestJDKStringConverters.java
+++ src/test/java/org/joda/convert/TestJDKStringConverters.java
@@ -331,7 +331,13 @@ public class TestJDKStringConverters {
     public void test_InetAddress() throws Exception {
         JDKStringConverter test = JDKStringConverter.INET_ADDRESS;
         doTest(test, InetAddress.class, InetAddress.getByName("1.2.3.4"), "1.2.3.4");
-        doTest(test, InetAddress.class, InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7334"), "2001:db8:85a3:0:0:8a2e:370:7334");
+        
+        InetAddress obj = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+        String str = test.convertToString(obj);
+        assertTrue(str.equals("2001:db8:85a3:0:0:8a2e:370:7334") || str.equals("2001:db8:85a3::8a2e:370:7334"));
+        assertEquals(obj, test.convertFromString(InetAddress.class, str));
+        assertEquals(obj, test.convertFromString(InetAddress.class, "2001:db8:85a3:0:0:8a2e:370:7334"));
+        assertEquals(obj, test.convertFromString(InetAddress.class, "2001:db8:85a3::8a2e:370:7334"));
     }
 
 //    @Test(expected=RuntimeException.class)
