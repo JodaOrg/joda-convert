3b5c3d083c89924723ded62d73afa208ba4a31d0
Stephen Colebourne <scolebourne@joda.org>
2022-08-24 18:02:08 +0100

    Update build scripts, dependabot

--- .github/website.sh
+++ .github/website.sh
@@ -16,7 +16,7 @@ cp -R ../site joda-convert/
 echo "## update..."
 git add -A
 git status
-git commit --message "Update joda-beans from CI: $GITHUB_ACTION"
+git commit --message "Update joda-convert from CI: $GITHUB_ACTION"
 
 echo "## push..."
 git push origin main
